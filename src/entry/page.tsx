adf4c247f76da577323130635d012a1965b1d113
Thomas Suebwicha <t.suebwicha@gmail.com>
2024-07-08 00:07:19 +1000

    Replaced next.js framework inplace of react framework

---
+++ src/entry/page.tsx
@@ -0,0 +1,8 @@
+// Logic: Fetch entries (GET)
+// http://localhost:8080/api/weight-entries
+
+// Post
+// http://localhost:8080/api/weight-entries
+
+// DELETE
+// http://localhost:8080/api/weight-entries/${id}
